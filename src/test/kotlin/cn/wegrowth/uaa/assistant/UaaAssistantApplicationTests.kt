1ded762773cedd73c170a63094a87aeced2ec61c
iCloud.Yin <xdyinay@qq.com>
2021-10-14 17:44:36 +0800

    init

---
+++ src/test/kotlin/cn/wegrowth/uaa/assistant/UaaAssistantApplicationTests.kt
@@ -0,0 +1,13 @@
+package cn.wegrowth.uaa.assistant
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UaaAssistantApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
